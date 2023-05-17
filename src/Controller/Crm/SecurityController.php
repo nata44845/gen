8d8eb8f0c3239f48e7f1a706cce079584a0a44c2
nata44845 <krppost@yandex.ru>
2023-05-17 14:36:03 +0700

    auth

---
+++ src/Controller/Crm/SecurityController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller\Crm;
+
+use App\Form\Type\LoginType;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+
+    private $mailer;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder, MailerInterface $mailer)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+         $this->mailer = $mailer;
+     }
+
+    public function login(Request $request, AuthenticationUtils $authUtils)
+    {
+//        $authUtils = $this->get('security.authentication_utils');
+        // last username entered by the user
+        $lastEmail = $authUtils->getLastUsername();
+
+        $form = $this->createForm(LoginType::class);
+        $form->setData(['email' => $lastEmail]);
+        $form->handleRequest($request);
+
+        $register = $request->get('register');
+        if (!$register) {
+            $register = false;
+        }
+
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        return $this->render('Security/login.html.twig', [
+            'lastEmail' => $lastEmail,
+            'error' => $error,
+            'flagRegister' => $register,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+}
---
+++ src/Form/Type/LoginType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LoginType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('remember', CheckboxType::class, [
+                'required' => false
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // 'data_class' => User::class
+        ]);
+    }
+}
