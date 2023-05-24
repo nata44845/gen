5124be19aa403488be5b79248d07fce5f9cad6e8
nata44845 <krppost@yandex.ru>
2023-05-24 13:26:42 +0700

    Заполнение шаблона списком администраторов

--- src/Controller/Crm/AdminController.php
+++ src/Controller/Crm/AdminController.php
@@ -3,12 +3,22 @@
 namespace App\Controller\Crm;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Crm\Admin;
 
 class AdminController extends AbstractController
 {
+  private $doctrine;
+
+  public function __construct(ManagerRegistry $doctrine) {
+    $this->doctrine = $doctrine;
+  }
   public function index()
   {
-    return $this->render('crm/admin/index.html.twig');
+    $admins = $this->doctrine->getRepository(Admin::class)->findAll();
+    return $this->render('crm/admin/index.html.twig',[
+      'admins' => $admins
+  ]);
   }
   
 }
